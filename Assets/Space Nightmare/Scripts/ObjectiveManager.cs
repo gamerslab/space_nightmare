04dd3e365ae1e1b54f160131578da7e51d3fefcc
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-12-10 03:53:28 +0100

    ObjectiveManager done. Shows the new objectives on screen. Use tab to see the current objective.

---
+++ Assets/Space Nightmare/Scripts/ObjectiveManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ObjectiveManager : MonoBehaviour {
+	static private ObjectiveManager m_Instance;
+	static public ObjectiveManager Instance { get { return m_Instance; } }
+
+	public Text currentObjective;
+	public string firstObjective = "Unknown objective";
+	public float showInterval = 2f;
+
+	float showTime = 0;
+	bool showed = false;
+
+	void Awake() {
+		m_Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		NewObjective(firstObjective);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Show Objectives")) {
+			currentObjective.gameObject.SetActive(true);
+		}
+
+		if(showTime > 0) {
+			showTime -= Time.deltaTime;
+		} else if(Input.GetButtonUp("Show Objectives") || showed) {
+			currentObjective.gameObject.SetActive(false);
+			showed = false;
+		}
+	}
+
+	public void NewObjective(string objective) {
+		currentObjective.text = objective;
+		currentObjective.gameObject.SetActive(true);
+		showed = true;
+		showTime = showInterval;
+	}
+}
---
+++ Assets/Space Nightmare/Scripts/ObjectiveOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveOnCollision : MonoBehaviour {
+	public string newObjective;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			ObjectiveManager.Instance.NewObjective(newObjective);
+			Destroy(gameObject);
+		}
+	}
+}

3a8fb1b4452d50449219310880c58ce28a4643f7
unknown <x.serra93@gmail.com>
2014-12-10 20:38:25 +0100

    Reactor Room and decoration changes

---
+++ Assets/Space Nightmare/Scripts/LightTilting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTilting : MonoBehaviour {
+	Light currentLight;
+	float accumulator = 0.0f;
+	// Use this for initialization
+	void Start () {
+		currentLight = transform.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (accumulator <= 0.0f)
+		{
+			int randomNumber = Random.Range (0, 3);
+			if (randomNumber == 0)
+			{
+				float currentIntensity = currentLight.intensity;
+
+				currentLight.intensity = 1f - currentIntensity;
+			}
+			
+			accumulator = 0.2f;
+		}
+		else
+		{
+			accumulator-=Time.deltaTime;
+		}
+	}
+}

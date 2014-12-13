feaa579ae3d194a69ed7254a288819c169dbdde3
unknown <x.serra93@gmail.com>
2014-12-13 22:52:16 +0100

    Ladder Climber

---
+++ Assets/Space Nightmare/Scripts/LadderClimber.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LadderClimber : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.gameObject.SendMessage("ladderScaling");
+		} else {
+			Debug.Log ("Alien detected from ladder");
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			other.gameObject.SendMessage("ladderLeaving");
+		} else {
+			Debug.Log ("Alien detected from ladder");
+		}
+	}
+	
+}

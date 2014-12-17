4e85439f211e80d4b7a9caaa7dea5a6ddbaf6dec
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-12-17 02:52:03 +0100

    Bomb target shines.

---
+++ Assets/Space Nightmare/Scripts/ShineEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShineEffect : MonoBehaviour {
+	public float frequency;
+
+	Renderer renderer;
+	float accum = 0;
+	bool ascending = false;
+	float interval;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer> ();
+		interval = 1f / frequency;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		accum += Time.deltaTime;
+		float value = accum / interval;
+
+		if(value > 1f) {
+			value = 0;
+			accum = 0;
+			ascending = !ascending;
+		}
+
+		if(ascending)
+			value = 1 - value;
+
+		Color color = new Color(value, value, value, renderer.materials[0].GetColor("_TintColor").a);
+
+		renderer.materials [0].SetColor ("_TintColor", color);
+	}
+}

2d69bca555ad837fb604f5cb61429f72c93ff75e
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-12-17 06:56:57 +0100

    Main menu done.

---
+++ Assets/Space Nightmare/Scripts/TextTilting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class TextTilting : MonoBehaviour {
+	Text text;
+	float accumulator = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (accumulator <= 0.0f) {
+			if (Random.Range(0, 3) == 0) {
+				Color currentColor = text.color;
+				currentColor.r = 1f-currentColor.r;
+				currentColor.g = 1f-currentColor.g;
+				currentColor.b = 1f-currentColor.b;
+				
+				text.color = currentColor;
+			}
+			
+			accumulator = 0.2f;
+		} else {
+			accumulator-=Time.deltaTime;
+		}
+	}
+}
